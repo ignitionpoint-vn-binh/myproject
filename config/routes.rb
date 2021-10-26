fbc9d8783c8dfbbe3d109678cf1841992f71a543
binh <khongancungbeo1994@gmail.com>
2021-10-26 19:35:08 +0700

    chap5 ennd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get 'signup', to: 'users#new'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
   root 'application#hello'
   # For detailson the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
