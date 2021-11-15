8a317aa03d065aaa32f91fc3fcbf4e6c0e64d6a8
binh <khongancungbeo1994@gmail.com>
2021-11-15 18:31:37 +0700

    suachap7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,14 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find_by(id: params[:id])
-
   	redirect_to root_path unless @user 
   end
 
   def create
-		@user = User.new(user_params) # Not the final implementation!
+		@user = User.new(user_params)
 		if @user.save
-			# Handle a successful save.
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to @user
-
 		else
 			render 'new'
 		end
@@ -24,7 +21,10 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password,
-		:password_confirmation)
+		params.require(:user).permit(:name, 
+																 :email, 
+																 :password,
+																 :password_confirmation
+		)
 	end
 end
