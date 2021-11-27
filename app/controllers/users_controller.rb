bc96878f31afbd5bb6a99b4c66fb90a820266818
binh <khongancungbeo1994@gmail.com>
2021-11-27 19:40:46 +0700

    chap14xong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :find_user , only: [:show ,:edit ,:update ,:destroy]
-  before_action :logged_in_user, only: [:edit, :update, :show, :index ,:destroy]
+  before_action :find_user , only: [:show ,:edit ,:update ,:destroy ,:following ,:followers]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+										:following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def new
@@ -44,6 +45,17 @@ class UsersController < ApplicationController
 		@user.destroy
 		flash[:success] = "User deleted"
 		redirect_to users_url
+	end
+
+	def following
+		@title = "Following"
+		@users = @user.following.paginate(page: params[:page])
+		render 'show_follow'
+	end
+	def followers
+		@title = "Followers"
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
 	end
 
 	private
